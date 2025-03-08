06c8e24f694a62526810b8cc9f356d0623127db5
unknown <bhavyavellanki@gmail.com>
2025-03-08 13:59:42 +0530

    2.0

---
+++ src/test/java/git_jenkin/tc2.java
@@ -0,0 +1,14 @@
+package git_jenkin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class tc2 {
+
+	@Test
+	public void test() {
+		System.out.println("testcase2");
+	}
+
+}
